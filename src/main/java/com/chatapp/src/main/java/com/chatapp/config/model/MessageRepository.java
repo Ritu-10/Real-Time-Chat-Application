f23b4245bd7c40a311c7b35fea19aad4ce0ce186
Ritu-10 <119799494+Ritu-10@users.noreply.github.com>
2025-09-23 20:02:20 +0530

    Create MessageRepository.java

---
+++ src/main/java/com/chatapp/src/main/java/com/chatapp/config/model/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.chatapp.repository;
+
+import com.chatapp.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
