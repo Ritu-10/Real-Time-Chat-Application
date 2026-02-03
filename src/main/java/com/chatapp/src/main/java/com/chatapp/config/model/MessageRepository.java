4c387dbffdfd5d9460d5e89341844392b1778dbf
Ritu-10 <119799494+Ritu-10@users.noreply.github.com>
2026-02-03 15:17:07 +0530

    Delete src/main/java/com/chatapp/src/main/java/com/chatapp/config directory

--- src/main/java/com/chatapp/src/main/java/com/chatapp/config/model/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chatapp.repository;
-
-import com.chatapp.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
